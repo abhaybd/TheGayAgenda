02a37df216bc2331db696f1880e5140e0ed8ba63
Abhay D <a.deshpande012@gmail.com>
2020-12-12 23:41:33 -0800

    JS whitelists, this is kinda messy

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-function setTheme(theme, oldTheme=undefined) {
+function setTheme(theme, oldTheme = undefined) {
     let header = $("header#gb");
     if (oldTheme) {
         let className = "theme-" + oldTheme;
@@ -9,16 +9,23 @@ function setTheme(theme, oldTheme=undefined) {
     header.addClass(className);
 }
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
+chrome.storage.onChanged.addListener(function (changes, namespace) {
     if ("theme" in changes) {
         setTheme(changes.theme.newValue, changes.theme.oldValue);
     }
 });
 
-$(function() {
+$(function () {
     $("[aria-label=Calendar]").first().children().last().text("The Gay Agenda");
 
-    chrome.storage.local.get("theme", function(obj) {
+    chrome.storage.local.get("theme", function (obj) {
         setTheme(obj.theme ?? "lgbt");
     });
+
+    setTimeout( function () {
+        $("header#gb :not(.gb_Ca *, [role=menu] *, [aria-label=\"Account Information\"] *, :has(*))")
+            .addClass("header-text");
+        $("header#gb div.BXL82c").addClass("header-text");
+        $("header#gb svg:not([aria-label=\"Account Information\"] *, #aso_search_form_anchor *)").addClass("header-svg");
+    }, 500);
 })
