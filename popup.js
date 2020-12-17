986fd5e46d1ac151cffed0c2092c4cfef014be88
Abhay D <a.deshpande012@gmail.com>
2020-12-17 15:04:09 -0800

    Add hex color previews, fixed bugs with caret while typing

--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
+// shamelessly copied from stack overflow
+$.fn.setCursorPos = function(pos) {
+    return this.each(function() {
+        if('selectionStart' in this) {
+            this.selectionStart = pos;
+            this.selectionEnd = pos;
+        } else if(this.setSelectionRange) {
+            this.setSelectionRange(pos, pos);
+        } else if(this.createTextRange) {
+            const range = this.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    });
+};
+
 $(function () {
     const inputTemplate = $("#custom-fields-template").remove().clone();
     inputTemplate.removeAttr("id");
@@ -30,21 +48,40 @@ $(function () {
         $("#" + theme).prop("checked", true);
 
         $("#apply-button").on("click", function () {
+            // TODO: add code to handle custom theme
             let theme = $("input[name=theme]:checked").val();
             chrome.storage.local.set({"theme": theme});
             console.log("New theme: " + theme);
         });
     });
 
-    $(document).on("input", ".color-input", function () {
+    $(document).on("input", ".color-input", function (e) {
         const elem = $(this);
-        let value = elem.val().toUpperCase();
-        value = value.replaceAll(/[^A-F]/g, "");
+        const rawVal = elem.val();
+        let value = rawVal.toUpperCase();
+        let cursorPos = e.target.selectionStart;
+
+        value = value.replaceAll(/[^0-9A-F]/g, "");
         if (value.length > 0 && !value.startsWith("#")) {
+            // we added a character when there previously was none, so the cursor should advance by one extra
+            if (!rawVal.startsWith("#")) {
+                cursorPos++;
+            }
             value = "#" + value;
         }
         value = value.substring(0, Math.min(value.length, 7));
+
+        // update the element attributes
         elem.val(value);
+        elem.attr("data-prev-val", value);
+
+        // update the cursor position
+        elem.setCursorPos(cursorPos);
+
+        // set the color of the color preview
+        let color = value.length === 0 ? "#000000" : value;
+        color = /^#([0-9A-F]{3}){1,2}$/.test(color) ? color : "transparent";
+        elem.siblings(".color-preview").css("background", color);
     });
 
     $("#add-color").on("click", function () {
