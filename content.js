77ad38a14b656cfcaffbeac4868b082f34a6456c
Abhay D <a.deshpande012@gmail.com>
2020-12-17 19:20:05 -0800

    Show custom themes in calendar

--- content.js
+++ content.js
@@ -4,20 +4,39 @@ function setTheme(theme, oldTheme = undefined) {
         let className = "theme-" + oldTheme;
         header.removeClass(className);
     }
-    // TODO: handle custom theme (dynamically create style sheet I think)
     let className = "theme-" + theme;
     console.log("Setting theme: " + className);
     header.addClass(className);
 }
 
+function defineCustomStyle(colors) {
+    let elem = $("style#tga-custom-style");
+    if (elem.length === 0) {
+        elem = $("<style>");
+        elem.attr("id", "tga-custom-style");
+        elem.appendTo(document.head);
+    }
+    elem.text(`.theme-custom { background: linear-gradient(90deg, ${colors.join(", ")}); }`);
+    console.log("Custom style added!");
+}
+
 chrome.storage.onChanged.addListener(function (changes) {
-    if ("theme" in changes) {
+    if (changes.theme) {
         setTheme(changes.theme.newValue, changes.theme.oldValue);
+    }
+    if (changes.custom_colors) {
+        defineCustomStyle(changes.custom_colors.newValue);
     }
 });
 
 $(function () {
     $("[aria-label=Calendar]").first().children().last().text("The Gay Agenda");
+
+    chrome.storage.local.get("custom_colors", function(obj) {
+        if (obj.custom_colors) {
+            defineCustomStyle(obj.custom_colors);
+        }
+    });
 
     chrome.storage.local.get("theme", function (obj) {
         setTheme(obj.theme ?? "lgbt");
